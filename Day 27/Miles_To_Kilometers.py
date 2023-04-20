126cbe1e3c799b495dd05bb49e22dd54bd4b0e02
Ergys Hila <ergysmania@gmail.com>
2023-04-20 08:07:41 -0400

    Day 27

---
+++ Day 27/Miles_To_Kilometers.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+window = Tk()
+window.title("Miles to killometers")
+window.minsize(width=225, height=100)
+Km_Label = Label(text="Km")
+Miles_Label = Label(text="Miles")
+is_equal_to = Label(text="is equal to")
+is_equal_to.grid(row=1, column=0)
+Km_Label.grid(row=1, column=2, pady=2)
+Miles_Label.grid(row=0, column=2, pady = 2)
+entry = Entry(width=8)
+entry.grid(row=0, column=1)
+text = Text(height=1, width=6)
+text.grid(row=1, column=1)
+
+
+
+def action():
+    text.delete("1.0", "end")
+    text.insert(END, (int(entry.get())* 1.60934))
+
+
+button = Button(text="Calculate", command=action)
+button.grid(row=3, column=1)
+
+window.mainloop()
