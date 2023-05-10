326fbab712748cd67ab1fc371008c06ac2a226f2
Ergys Hila <ergysmania@gmail.com>
2023-05-10 12:13:44 -0400

    Day 39

--- Day 39/data_manager.py
+++ Day 39/data_manager.py
@@ -1,3 +1,28 @@
+import requests
+from pprint import pprint
+
+url = "https://api.sheety.co/6cf584d0a1f8cda1ace24ec050db046c/αντίγραφο τουFlightDeals/prices"
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=url)
+        response.raise_for_status()
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
