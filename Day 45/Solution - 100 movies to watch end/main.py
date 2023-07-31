1ab3bcf61d8c37d1928c7bcdb4819a4f2b78b8de
ERGYS HILA <ergysmania@gmail.com>
2023-07-31 19:11:32 -0400

    Update main.py

--- Day 45/Solution - 100 movies to watch end/main.py
+++ Day 45/Solution - 100 movies to watch end/main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
+#main
 
 response = requests.get(URL)
 website_html = response.text
