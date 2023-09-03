63c784b0c3a7e37178d95083f52bc40301fab9c4
ERGYS HILA <ergysmania@gmail.com>
2023-09-02 23:31:38 -0400

    Update main.py

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,7 +1,7 @@
 
 import pandas
 
-data = pandas.read_csv( r'Day 26\nato_phonetic_alphabet.csv')
+data = pandas.read_csv( r' Day 26\nato_phonetic_alphabet.csv')
 #TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
