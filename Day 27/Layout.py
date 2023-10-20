d204b9ded0dbd822fabb15520397d294b64ad577
ERGYS HILA <ergysmania@gmail.com>
2023-10-19 22:16:51 -0400

    Update Layout.py

--- Day 27/Layout.py
+++ Day 27/Layout.py
@@ -9,7 +9,7 @@ window.title("Widget Examples")
 window.minsize(width=500, height=500)
 
 #Labels
-label = Label(text="This is old text")
+label = Label( text="This is old text")
 label.config(text="This is new text")
 label.pack()
 
